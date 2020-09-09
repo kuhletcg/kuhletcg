a03a184ea8b8bf3a5705c22698f1f10c7037cd24
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-09-09 08:29:20 +0000

    changing background color using js

---
+++ practice/index.js
@@ -0,0 +1,6 @@
+// changing backgroundColor using js
+
+function changeStyle(){
+var text = document.getElementById("para1").style.backgroundColor = "Blue";
+var text = document.getElementById("para1").style.fontSize = "Oswald";
+}
